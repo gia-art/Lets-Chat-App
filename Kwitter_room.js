3bae5ca1b7be7f9f4a3d5fa8a18d80bfb0d489bd
gia-art <77082206+gia-art@users.noreply.github.com>
2021-10-18 19:51:13 -0500

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCje5JRq2XFbPkENmHT-RnxGG3TvUwIkU",
+    authDomain: "kwitter-861a3.firebaseapp.com",
+    databaseURL: "https://kwitter-861a3-default-rtdb.firebaseio.com",
+    projectId: "kwitter-861a3",
+    storageBucket: "kwitter-861a3.appspot.com",
+    messagingSenderId: "789567656698",
+    appId: "1:789567656698:web:2ac7bd2956f729a3672f43"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+ 
